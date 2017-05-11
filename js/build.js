75c80689d5a6c3073a0fe9ee863b8debdb23aa44
Zhao Ning <ning.zhao@zymobi.com>
2017-05-11 16:38:57 +0800

    ‘md文件编译成html文件’

---
+++ js/build.js
@@ -0,0 +1,12 @@
+const fs = require('fs'); //file system
+const path = require('path');
+
+fs.readdir('../markdown', function(error, files){
+  for(var i = 0, len = files.length; i < len; i++) {
+    var p = path.join('../markdown', files[i]);
+    var markdown = fs.readFileSync(p).toString();
+    var template = fs.readFileSync('../template.html').toString();
+    var result = template.replace('%content%', markdown);
+    fs.writeFileSync('../' + files[i] + '.html', result);
+  }
+});
